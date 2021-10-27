e958c4c16b4936220d80aff5c3c8481cb0bf8c2d
hyflersd <hyflersd@github.com>
2021-10-27 14:01:13 -0400

    learned how to revert

---
+++ src/appAutoload/index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+use AppAutoload\PaymentGateway\Stripe\{Transaction, CustomerProfile};
+
+
+require __DIR__ . '/../vendor/autoload.php';
+$id = new \Ramsey\Uuid\UuidFactory();
+echo $id->uuid4();
+var_dump(new Transaction());
